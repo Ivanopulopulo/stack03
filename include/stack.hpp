c3bd1e09ac9e8708fefa07d9618ff0513d85a1a9
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-10-23 21:53:41 +0300

    Add files via upload

---
+++ include/stack.hpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+class stack
+{
+public:
+	stack() noexcept;
+	~stack() noexcept;
+	stack(stack<T> const&)/*no safety*/;
+	stack& operator=(stack<T> const&)/*no safety*/;
+	size_t count()const noexcept;
+	void push(T const&)/*no safety*/;
+	void pop()/*strong*/;
+	T top()const /*strong*/;
+	void print(std::ostream&stream)const /*strong*/;
+	void swap(stack<T>&)noexcept;
+	bool empty()const noexcept;
+private:
+	T * array_;
+	size_t array_size_;
+	size_t count_;
+};
+
+template <typename T> 
+stack<T>::stack() noexcept : array_{ nullptr }, array_size_{ 0 }, count_{ 0 } {}
+template <typename T>
+stack<T>::~stack()noexcept
+{
+	delete[] array_;
+}
+template <typename T>
+stack<T>::stack(stack<T> const& other)
+{
+	array_size_ = other.array_size_;
+	count_ = other.count_;	
+	std::copy(other.array_, other.array_ + count_, array_);
+}
+template <typename T>
+stack<T>& stack<T>::operator=(stack<T> const & other)
+{
+	if (&other != this)
+		stack(other).swap(*this);
+	return *this;
+}
+template <typename T>
+size_t stack<T>::count()const noexcept
+{
+	return count_;
+}
+template <typename T>
+void stack<T>::push(T const & value)
+{
+	if (empty())
+	{
+		array_size_ = 1;
+		array_ = new T[array_size_]();
+	}
+	else if (array_size_ == count_)
+	{
+		array_size_ *= 2;
+		T * new_array = new T[array_size_]();
+		std::copy(array_, array_ + count_, new_array);
+		delete[] array_;
+		array_ = new_array;
+	}
+	array_[count_++] = value;
+}
+template <typename T>
+void stack<T>::pop()
+{
+	if (empty())
+		throw "Stack is empty";
+	--count_;
+}
+template <typename T>
+T stack<T>::top()const 
+{
+	if (empty())
+		throw "Stack is empty";
+	else return array_[count_ - 1];
+}
+template <typename T>
+void stack<T>::print(std::ostream&stream)const 
+{
+	if(!empty())
+	{
+	        for (unsigned int i = 0; i < count_; ++i)
+		        stream << array_[i] << " ";
+	        stream << std::endl;
+	}
+}
+template <typename T>
+void stack<T>::swap(stack<T>& other)noexcept
+{
+	std::swap(array_, other.array_);
+	std::swap(array_size_, other.array_size_);
+	std::swap(count_, other.count_);
+}
+template <typename T>
+bool stack<T>::empty()const noexcept
+{
+	return (count_ == 0);
+}
---
+++ sources/stack.cpp
@@ -0,0 +1,51 @@
+#include "stack.hpp"
+#include <stdlib.h>
+#include <string>
+
+int main()
+{
+	stack<int> Stack;
+	std::string str;
+	unsigned int i = 0;
+
+	std::cout << "Add an element: +<Element>\nShow the last element: ?\nPop the last element: -\nShow the stack: =\n\n";
+	std::getline(std::cin, str);
+
+	while (str[i] != '\0')
+	{
+		switch (str[i])
+		{
+		case '+':
+		{
+					if (str[i + 1] == ' ' || str[i + 1] == '\0')
+						std::cout << "Error! Error has occurred while reading arguments\n";
+					else
+					{
+						Stack.push(stoi(str.substr(i + 1)));
+					}
+					break;
+		}
+		case '?':
+		{
+					std::cout << Stack.top();
+					std::cout << std::endl;
+					break;
+		}
+		case '-':
+		{
+					Stack.pop();
+					break;
+		}
+		case '=':
+		{
+					Stack.print(std::cout);
+					break;
+		}
+		defoult: break;
+		}
+		i++;
+	}
+
+	system("pause");
+	return 0;
+}
