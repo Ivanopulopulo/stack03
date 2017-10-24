c27b9430b4d9bd2025d54cac54de32e2055d5e4a
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-10-24 08:16:02 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -14,7 +14,7 @@ public:
 	void push(T const&)/*no safety*/;
 	void pop()/*strong*/;
 	T top()const /*strong*/;
-	void print(std::ostream&stream)const /*strong*/;
+	void print const /*strong*/;
 	void swap(stack<T>&)noexcept;
 	bool empty()const noexcept;
 private:
@@ -82,7 +82,7 @@ T stack<T>::top()const
 	else return array_[count_ - 1];
 }
 template <typename T>
-void stack<T>::print(std::ostream&stream)const 
+void stack<T>::print const 
 {
 	if(!empty())
 	{
